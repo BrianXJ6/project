ad8b11d9ea6f48bb1b311ea9f49918e8705b4c0c
Brian Ferreira <brianferreira15@hotmail.com>
2021-07-13 21:18:05 -0300

    Building

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,11 @@
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
   \*****************************/
-/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _global_plugins_snotify__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./global/plugins/snotify */ "./resources/js/global/plugins/snotify/index.js");
 // Vue
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js").default; // Bootstrap and jQuery
 
@@ -16,7 +19,9 @@ __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap
 
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
-window.axios.defaults.headers.common['Accept'] = 'application/json'; // Vue auto components
+window.axios.defaults.headers.common['Accept'] = 'application/json'; // Global plugins
+
+ // Vue auto components
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 // Vue single components
@@ -24,6 +29,28 @@ window.axios.defaults.headers.common['Accept'] = 'application/json'; // Vue auto
 // Start
 
 var app = new Vue().$mount('#app');
+
+/***/ }),
+
+/***/ "./resources/js/global/plugins/snotify/index.js":
+/*!******************************************************!*\
+  !*** ./resources/js/global/plugins/snotify/index.js ***!
+  \******************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js");
+/* harmony import */ var vue_snotify__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue-snotify */ "./node_modules/vue-snotify/vue-snotify.esm.js");
+
+
+var options = {
+  toast: {
+    timeout: 3000,
+    position: vue_snotify__WEBPACK_IMPORTED_MODULE_0__.SnotifyPosition.rightTop
+  }
+};
+vue__WEBPACK_IMPORTED_MODULE_1__.default.use(vue_snotify__WEBPACK_IMPORTED_MODULE_0__.default, options); // <vue-snotify></vue-snotify>
 
 /***/ }),
 
